a6f03588e764317d1fcbac29f820d60a4c791a57
Vraj Patel <vrajptl0610@gmail.com>
2025-04-27 18:44:21 -0400

    changes for coda to BKMS integration

---
+++ app.py
@@ -0,0 +1,118 @@
+from coda import format_data
+from bkms import update_sheet
+import customtkinter
+from threading import Thread
+
+class UpdateFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, title, value):
+        super().__init__(master, label_text=title, height=75)
+        self.grid_columnconfigure(0, weight=1)
+        self.value = value
+        self.label = customtkinter.CTkLabel(self, text=self.value, fg_color="transparent")
+        self.label.grid(row=0, column=0, pady=(0, 0))
+
+    def change_item(self, new_text):
+        self.label.configure(text=new_text)
+
+class WeekdayFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.title = title
+        self.radiobuttons = []
+        self.variable = customtkinter.StringVar(value="")
+
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+
+        for i, value in enumerate(self.values):
+            radiobutton = customtkinter.CTkRadioButton(self, text=value, value=value, variable=self.variable)
+            radiobutton.grid(row=i + 1, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.radiobuttons.append(radiobutton)
+
+    def get(self):
+        return self.variable.get()
+
+    def set(self, value):
+        self.variable.set(value)
+
+class WeekDateFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.title = title
+        self.entries = []
+
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+
+        for i, value in enumerate(self.values):
+            entry = customtkinter.CTkEntry(self, placeholder_text=value)
+            entry.grid(row=i + 1, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.entries.append(entry)
+
+    def get(self):
+        filled_entries = []
+        for entry in self.entries:
+            if not entry.get() == "":
+                filled_entries.append(entry.get())
+        return filled_entries
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("750x350")
+        self.grid_columnconfigure((0), weight=1)
+        self.title("CODA to BKMS Bot")
+
+        self.weekdate_frame = WeekDateFrame(self, "Date", values=["Date (March 12)"])
+        self.weekdate_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
+        self.weekday_frame = WeekdayFrame(self, "Group", values=["Saturday K1", "Saturday K2", "Sunday K1", "Sunday K2"])
+        self.weekday_frame.grid(row=0, column=1, padx=(0, 0), pady=(10, 0), sticky="nsew")
+
+        self.sabha_held_frame = WeekdayFrame(self, "Was Sabha Held?", values=["Yes", "No"])
+        self.sabha_held_frame.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
+        self.p2_guju_frame = WeekdayFrame(self, "Was P2 in Guju?", values=["Yes", "No"])
+        self.p2_guju_frame.grid(row=1, column=1, padx=10, pady=(10, 0), sticky="nsew")
+
+        self.update_frame = UpdateFrame(master=self, title="Bot Updates", value="Bot is up and ready")
+        self.update_frame.grid(row=0, column=2, rowspan=2, padx=(10), pady=(10, 0), sticky="nsew")
+        self.update_frame._scrollbar.configure(height=0)
+
+        self.button = customtkinter.CTkButton(self, text="Run Bot", command=self.on_button_clicked)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=3)
+
+        self.progress = customtkinter.CTkProgressBar(self, orientation="horizontal")
+        self.progress.grid(row=4, column=0, padx=10, pady=(0,10), sticky="ew", columnspan=3)
+        self.progress.set(0)
+
+    def on_button_clicked(self):
+        btn_thread = Thread(target=self.button_callback, daemon=True)
+        btn_thread.start()
+
+    def button_callback(self):
+        date = self.weekdate_frame.get()[0]
+        weekday = self.weekday_frame.get()
+        sabha_held = self.sabha_held_frame.get()
+        p2_guju = self.p2_guju_frame.get()
+        try:
+            week = self.weekdate_frame.get()[1]
+        except:
+            week = None
+
+        if date and weekday and sabha_held and p2_guju:
+            self.update_frame.change_item(f"Weekday: {weekday}\nDate: {date}")
+            attendance, attendance_count = format_data(weekday, date)
+            self.update_frame.change_item(f"{attendance_count} Kishores Found\nMoving on to BKMS Reporting")
+            self.progress.set(0.75)
+            update_sheet(attendance, weekday, week, sabha_held, p2_guju)
+            self.progress.set(1)
+        else:
+            self.update_frame.change_item(f"Try Again, make sure you\nselect all required fields")
+
+app = App()
+app.mainloop()
---
+++ bkms.py
@@ -0,0 +1,198 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import datetime
+
+today = datetime.date.today()
+week_number = today.isocalendar().week
+
+options = Options()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_experimental_option("detach", True)
+service = webdriver.ChromeService()
+
+day = "saturday"
+url = "https://bk.na.baps.org/ssologin"
+
+def update_sheet(attended_kishores, day: str, week, sabha_held: str, p2_guju: str):
+   driver = webdriver.Chrome(service=service, options=options)
+
+   driver.get(url)
+
+   time.sleep(1)
+   print("Logging In to BKMS")
+   user_id = driver.find_element(By.ID, "user_id") 
+   user_email = driver.find_element(By.ID, "email") 
+   user_password = driver.find_element(By.ID, "password") 
+
+   user_id.send_keys("3001")
+   time.sleep(0.5)
+   user_email.send_keys("vrajptl0610@gmail.com")
+   time.sleep(0.5)
+   user_password.send_keys("12345678")
+   print("DO THE CAPTCHA (you have 30 sec)")
+   
+   time.sleep(30)
+   driver.find_element(By.CLASS_NAME, "btn-primary").click()
+   time.sleep(2)
+   driver.get("https://bk.na.baps.org/admin/reports/reportweeksabhaattendance")
+   time.sleep(3)
+   
+   print("Logged in (if captcha was done)")
+   
+   if day.lower() == "saturday k1":
+      print("Selecting sabha wing")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[3]/select/option[4]').click()
+      time.sleep(1)
+      print("Selected Sabha wing")
+      time.sleep(2)
+      print("Selecting year")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[4]/select/option[9]').click()
+      time.sleep(1)
+      print("Selected year")
+      time.sleep(2)
+      print("Selecting Sabha Week")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[5]/select/option[{week_number + 1}]').click()
+      time.sleep(1)
+      print("Selected Sabha Week")
+      time.sleep(2)
+      print("Selecting Saturday K1")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[2]/div[2]/div/table/tbody/tr[1]/td[9]/div/span/a').click()
+      print("Selected Saturday K1")
+   elif day.lower() == "saturday k2":
+      print("Selecting sabha wing")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[3]/select/option[4]').click()
+      time.sleep(1)
+      print("Selected Sabha wing")
+      time.sleep(2)
+      print("Selecting year")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[4]/select/option[9]').click()
+      time.sleep(1)
+      print("Selected year")
+      time.sleep(2)
+      print("Selecting Sabha Week")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[5]/select/option[{week_number + 1}]').click()
+      time.sleep(1)
+      print("Selected Sabha Week")
+      time.sleep(2)
+      print("Selecting Saturday K2")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[2]/div[2]/div/table/tbody/tr[2]/td[9]/div/span/a').click()
+      print("Selected Saturday K2")
+   elif day.lower() == "sunday k1":
+      print("Selecting sabha wing")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[3]/select/option[4]').click()
+      time.sleep(1)
+      print("Selected Sabha wing")
+      time.sleep(2)
+      print("Selecting year")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[4]/select/option[9]').click()
+      time.sleep(1)
+      print("Selected year")
+      time.sleep(2)
+      print("Selecting Sabha Week")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[5]/select/option[{week_number + 1}]').click()
+      time.sleep(1)
+      print("Selected Sabha Week")
+      time.sleep(2)
+      print("Selecting Sunday K1")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[2]/div[2]/div/table/tbody/tr[3]/td[9]/div/span/a').click()
+      print("Selected Sunday K1")
+   elif day.lower() == "sunday k2":
+      print("Selecting sabha wing")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[3]/select/option[4]').click()
+      time.sleep(1)
+      print("Selected Sabha wing")
+      time.sleep(2)
+      print("Selecting year")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[4]/select/option[9]').click()
+      time.sleep(1)
+      print("Selected year")
+      time.sleep(2)
+      print("Selecting Sabha Week")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div[1]/div[5]/select/option[{week_number + 1}]').click()
+      time.sleep(1)
+      print("Selected Sabha Week")
+      time.sleep(2)
+      print("Selecting Sunday K2")
+      driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[2]/div[2]/div/table/tbody/tr[4]/td[9]/div/span/a').click()
+      print("Selected Sunday K2")
+   
+   time.sleep(3)
+
+   #Check whether sabha was held or not
+   if sabha_held.lower() == "yes":
+      driver.find_element(By.XPATH, '/html/body/div[2]/div/section[2]/div[1]/form/div[1]/label[1]/div/ins').click()
+   else:
+      driver.find_element(By.XPATH, '/html/body/div[2]/div/section[2]/div[1]/form/div[1]/label[2]/div/ins').click()
+   time.sleep(2)
+   
+   #Karyakar Meeting
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/form/div[3]/div/div[1]/label[2]/div/ins').click()
+   #2 Week Prep Cycle
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/form/div[3]/div/div[2]/label[3]/div/ins').click()
+   #Pre-Sabha Review
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/form/div[3]/div/div[3]/label[2]/div/ins').click()
+   #Post-Sabha Review
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/form/div[3]/div/div[4]/label[2]/div/ins').click()
+   #Culture Change
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/form/div[3]/div/div[5]/label[3]/div/ins').click()
+   #Bapa’s Ashirwaad
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div/div[1]/label[2]/div/ins').click()
+   #Dhoon & Prarthana
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div/div[2]/label[2]/div/ins').click()
+   #Presentation 1
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div/div[3]/label[2]/div/ins').click()
+   #Kirtan
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div/div[4]/label[2]/div/ins').click()
+   #Presentation 2
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div/div[5]/label[2]/div/ins').click()
+
+   #Presentation done in Gujarati or not?
+   if p2_guju.lower() == "yes":
+      driver.find_element(By.XPATH, '/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div/div[6]/label[2]/div/ins').click()
+   else:
+      driver.find_element(By.XPATH, '/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div/div[6]/label[3]/div/ins').click()
+
+   #Sabha Goshti
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[2]/form/div/div[7]/label[3]/div/ins').click()
+
+   #Mark all as absent
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[2]/div[1]/span/a').click()
+   print("Selecting Kishores")
+   time.sleep(2)
+
+
+   all_kishores = driver.find_elements(By.XPATH, '//tr[@role="row"]')
+   updated_kishores = []
+   no_found_kishores = []
+   
+   index = -1
+   count = 0
+   for element in all_kishores:
+    index += 1
+    name = element.text
+    name_parts = name.split()
+    bkid = name_parts[0]
+
+    if bkid in attended_kishores:
+        count += 1
+        radio_button = element.find_element(
+            By.XPATH,
+            f'/html/body/div[2]/div/section[2]/div[2]/div[2]/div/div[2]/div/table/tbody/tr[{index}]/td[10]/label/input'
+        )
+        radio_button.click()
+        updated_kishores.append(bkid)
+         
+   for kishore in attended_kishores:
+      if not kishore in updated_kishores:
+         no_found_kishores.append(kishore)
+
+   print("⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯")
+   print(f'These kishores attended sabha but were not in BKMS: {no_found_kishores}')
+   print(f"Everythings Done!\nClicked {count} kishores")
+   time.sleep(2)
+   driver.find_element(By.XPATH, f'/html/body/div[2]/div/section[2]/div[1]/div[4]/form/div[3]/div/input[1]').click()
+   print("Saved Changes and All Kishores who attended sabha are marked as present")
