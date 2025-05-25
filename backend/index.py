806ec4f9313a571e3dabc5db666903a42fc73a00
Vraj Patel <vrajptl0610@gmail.com>
2025-05-25 01:24:57 -0400

    updated UI link with https

--- backend/index.py
+++ backend/index.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "vrajpatelbkms.vercel.app"
+        "https://vrajpatelbkms.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
