ad4483542e42a65a9c0c072918f53f42aedd0e01
Vraj Patel <vrajptl0610@gmail.com>
2025-05-25 01:15:52 -0400

    fixed UI url

--- backend/index.py
+++ backend/index.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://vrajpatelbkms-p3bpr37yy-vrajp610s-projects.vercel.app"
+        "vrajpatelbkms.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
